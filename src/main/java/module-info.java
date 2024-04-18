5284f6f8746cba0f75c6d088316ccfce091fea70
PetritCena <k12217828@students.jku.at>
2024-04-18 22:54:00 +0200

    module-info.java and material_theme_project_new.xml revised

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,11 @@
-module org.example.bankingapplication {
+module org.example.app {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
     requires de.jensd.fx.glyphs.fontawesome;
+    requires java.desktop;
 
 
-    opens com.jmc.bankingapplication to javafx.fxml;
-    exports com.jmc.bankingapplication;
+    opens com.jmc.app.Controllers to javafx.fxml;
+    exports com.jmc.app;
 }
